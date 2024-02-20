b4394de43ab96772cbb2e2c2b4ba9b7bd78d3c71
Almat1988 <121474004+Almat1988@users.noreply.github.com>
2024-02-20 14:29:24 +0600

    Create Main.php

---
+++ src/EssentialsServer/Main.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace EssentialsServer;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+  public function onEnable(){
+  }
+  public function onLoad(){
+  }
+  public function onDisable(){
+  }
+}
+  
